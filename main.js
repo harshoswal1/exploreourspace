b76206cb5e02f9c70841d3e4965803e31a386537
harshoswal1 <harshpurav@gmail.com>
2026-04-05 11:29:03 +0530

    force working satellite data

--- main.js
+++ main.js
@@ -118,6 +118,11 @@ createHomePage(() => {
   asteroidStatusBadge = createAsteroidStatusBadge();
   satelliteStatusBadge = createSatelliteStatusBadge();
   exitFollowBtn = createFollowExitButton();
+
+  // Move the listener here so it only attaches after the button is created
+  exitFollowBtn.addEventListener('click', () => {
+    clearFollowMode();
+  });
   
   // Initialize badge states now that they exist
   if (satelliteStatusBadge) {
