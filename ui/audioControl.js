9d9428d9ec62ec3ffe277e9afcfcb9086faf350b
harshoswal1 <harshpurav@gmail.com>
2026-04-05 10:43:31 +0530

    force working satellite data

--- ui/audioControl.js
+++ ui/audioControl.js
@@ -19,7 +19,7 @@ export function createAudioControl() {
   
   // Positioned in the top-right area, between the Live Badge and the Search Bar
   button.style.top = isMobile ? '12px' : '27px';
-  button.style.right = isMobile ? '70px' : '520px';
+  button.style.right = isMobile ? '87px' : '520px';
   
   button.style.width = isMobile ? '32px' : '40px';
   button.style.height = isMobile ? '32px' : '40px';
